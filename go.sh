563bb39209f85ed5d371b3063be5fa9123fe3b28
Freddie <freddie@thing.engineering>
2025-12-07 18:36:59 -0800

    Add progress text to go.sh

--- go.sh
+++ go.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 
+echo "Deleting existing icons..."
 rm -rf [1-5]*
+echo "Resizing..."
 python3 resize.py
+echo "Generating WebP icons..."
 find [1-5]* -name '*.png' \! -type l -print0 | parallel -q0 cwebp -q 85 -quiet "{}" -o "{.}.webp"
+echo "Fixing links..."
 python3 links.py
+echo "Creating tar..."
 tar cf icons.tar [1-5]*
